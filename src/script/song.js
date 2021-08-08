108e12451657452c938ecb097c93afd9b211ee02
ali salemi <alisalemmi@outlook.com>
2021-08-08 09:09:54 +0430

    add suggestion to song.js

--- src/script/song.js
+++ src/script/song.js
@@ -1,3 +1,6 @@
+const suggestionTemplate = document.querySelector('template#suggestion');
+const suggestionList = document.querySelector('.song__item__list');
+
 function renderSong({ name, artist, lyrics, file, cover }) {
   document.querySelector('.song__name').innerHTML = name;
   document.querySelector('.song__singer').innerHTML = artist;
@@ -7,4 +10,21 @@ function renderSong({ name, artist, lyrics, file, cover }) {
   document.querySelector('.song__lyrics').innerHTML = `<li>${lyrics
     .split('\n')
     .join('</li><li>')}</li>`;
+}
+
+function addSuggestion({ id, name, artist, cover, duration }) {
+  const song = suggestionTemplate.content.cloneNode(true);
+
+  const a = song.querySelector('a');
+  a.href = `./song.html?id=${id}`;
+
+  const img = a.querySelector('.song__item__image');
+  img.src = cover;
+  img.alt = name;
+
+  a.querySelector('.song__item__name').innerText = name;
+  a.querySelector('.song__item__singer').innerText = artist;
+  a.querySelector('.song__item__duration').innerText = duration;
+
+  suggestionList.appendChild(song);
 }
